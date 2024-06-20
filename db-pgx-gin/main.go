72a2be8969671170bc9de72e77976d540334a79b
mufti-shiddiq <muftishid@gmail.com>
2024-06-20 09:24:25 +0700

    feat: integrate gin with postgreSQL

---
+++ db-pgx-gin/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	// "fmt"
+	"gin/db-pgx-gin/handler"
+	"gin/db-pgx-gin/repository/postgres_pgx"
+	"gin/db-pgx-gin/router"
+	"gin/db-pgx-gin/service"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+
+	pgxPool, err := connectDB("postgresql://postgres:postgres@localhost:5432/go-advanced")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// fmt.Println(pgxPool)
+	// setup service
+
+	// slice db is disabled. uncomment to enabled
+	// var mockUserDBInSlice []entity.User
+	// _ = slice.NewUserRepository(mockUserDBInSlice)
+
+	// pgx db is enabled. comment to disabled
+	userRepo := postgres_pgx.NewUserRepository(pgxPool)
+	userService := service.NewUserService(userRepo)
+	userHandler := handler.NewUserHandler(userService)
+
+	// Routes
+	router.SetupRouter(r, userHandler)
+
+	// Run the server
+	log.Println("Running server on port 8080")
+	r.Run(":8080")
+}
+
+func connectDB(dbURL string) (postgres_pgx.PgxPoolIface, error) {
+	return pgxpool.New(context.Background(), dbURL)
+}
