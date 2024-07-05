6329201e7bb8c07b7f867648be43100041c46509
mufti-shiddiq <muftishid@gmail.com>
2024-07-05 09:57:17 +0700

    feat: add GRPC with sayHello

---
+++ grpc-introduction/client/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	pb "gin/grpc-introduction/proto/helloworld/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
+
+func main() {
+	runClient()
+}
+
+func runClient() {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	greeterClient := pb.NewGreeterServiceClient(conn)
+
+	name := "world"
+	r, err := greeterClient.SayHello(context.Background(), &pb.SayHelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+}
---
+++ grpc-introduction/server/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	pb "gin/grpc-introduction/proto/helloworld/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"log"
+	"net"
+	"time"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServiceServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.SayHelloRequest) (*pb.SayHelloResponse, error) {
+	hello := &pb.Hello{
+		Id:        0,
+		Name:      "test nama",
+		Active:    false,
+		Type:      pb.HelloType_HELLO_ACTIVE,
+		Schools:   []string{"sd 1", "smp 2"},
+		CreatedAt: timestamppb.New(time.Now().Add(-4 * time.Hour)), // 4 hours ago
+		UpdatedAt: timestamppb.New(time.Now()),
+	}
+	fmt.Println(hello)
+	return &pb.SayHelloResponse{Message: fmt.Sprintf("Hello World %s", in.GetName()), Hello: hello}, nil
+}
+
+func main() {
+	runServer()
+}
+
+func runServer() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServiceServer(s, &server{})
+	log.Println("Server is running on port :50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
